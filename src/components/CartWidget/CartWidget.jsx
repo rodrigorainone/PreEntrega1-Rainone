6818be0eda5411e07b520782ede4e343037ec40a
Tuky <rodri_cjs22@hotmail.com>
2023-01-22 09:44:20 -0300

    arreglo Pre entrega 1

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,12 +3,8 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 const CartWidget = ({cantCarrito}) => {
     return (
         <>
-            <a href="http://" id="tuky"><FontAwesomeIcon  icon={faCartShopping} /></a>
-            
-            
-            
-            <p className='text-white'>{cantCarrito}</p>
-            
+            <a href="http://" id="tuky"><FontAwesomeIcon  icon={faCartShopping} /></a>          
+            <p className='text-white'>{cantCarrito}</p>            
         </>
     );
 }
