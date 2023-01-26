2ccabab18e20e0c82b76392fa68dd34917cde5b0
Tuky <rodri_cjs22@hotmail.com>
2023-01-26 00:12:10 -0300

    card en la pagina

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,17 +1,22 @@
 import {useState,useEffect}  from 'react'
+import Itemlist from '../ItemList/Itemlist';
 
-const ItemListContainer = () => {
-    const [productos,setProductos] = useState([]);
-    useEffect(()=>{
-        fetch('./json/productos.json')       
-        .then(response => response.json())
-        .then(products => console.log(products));
-        
-    },[]);
+const ItemListContainer = () => {    
+    
+        const [productos,setProductos] = useState([]);
 
+        useEffect(()=>{
+            fetch('./PreEntrega1-Rainone/json/productos.json')
+                .then(response => response.json())
+                .then(products => {                    
+                    const productsList = Itemlist({products})
+                    setProductos(productsList)               
+                })
+        },[]);   
+       
     return (        
         <div >
-            
+            {productos}
         </div>
     );
 }
